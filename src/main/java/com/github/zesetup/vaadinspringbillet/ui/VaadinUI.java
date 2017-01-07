ceb3ec95fbd967acc11ae3f143c183b3614ff3cf
Maksim <zerosetup@gmail.com>
2017-01-08 00:16:42 +0200

    added vaadin

---
+++ src/main/java/com/github/zesetup/vaadinspringbillet/ui/VaadinUI.java
@@ -0,0 +1,17 @@
+package com.github.zesetup.vaadinspringbillet.ui;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.UI;
+
+@SpringUI
+@Theme("valo")
+public class VaadinUI extends UI {
+	 @Override
+	 protected void init(VaadinRequest request) {
+		 setContent(new Button("Click me", e->Notification.show("Hello Spring+Vaadin user!")));
+	 }
+}
