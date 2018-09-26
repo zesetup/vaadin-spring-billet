09688fe99f78316ff606cebf7bcb1515c5eeecda
maks <zesetup@ya.ru>
2018-09-26 23:07:39 +0200

    to 11...

--- src/main/java/com/github/zesetup/vaadinspringbillet/ui/vaadinUI.java
+++ src/main/java/com/github/zesetup/vaadinspringbillet/ui/vaadinUI.java
@@ -4,16 +4,25 @@ import com.github.zesetup.vaadinspringbillet.model.Employee;
 import com.github.zesetup.vaadinspringbillet.service.EmployeeService;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.provider.DataProvider;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
+
+import java.util.Random;
+import java.util.stream.IntStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Route
-public class vaadinUI {
+public class vaadinUI extends VerticalLayout {
 
   @Autowired
   private EmployeeService employeeService;
@@ -50,7 +59,7 @@ public class vaadinUI {
       employeeEditor.editEmployee(employee);
       employeeEditor.setVisible(true);
     });
-    Button editBtn = new Button("Edit", VaadinIcons.EDIT);
+    Button editBtn = new Button("Edit", VaadinIcon.EDIT.create());
 
     editBtn.addClickListener(e -> {
       if (!grid.getSelectedItems().isEmpty()) {
@@ -66,16 +75,24 @@ public class vaadinUI {
     Label counterLabel = new Label();
     HorizontalLayout actions = new HorizontalLayout(filterField, addNewBtn, editBtn);
     VerticalLayout verticalLayout = new VerticalLayout(actions, grid, counterLabel);
-    setContent(verticalLayout);
+    add(verticalLayout);
     filterField.addValueChangeListener(e -> {
       grid.setDataProvider((sortOrder, offset, limit) -> employeeService.findWithFilter(sortOrder,
           offset, limit, e.getValue()), () -> employeeService.countWithFilter(e.getValue()));
-      counterLabel.setValue("Size:" + employeeService.countWithFilter(e.getValue()));
+      counterLabel.setText("Size:" + employeeService.countWithFilter(e.getValue()));
     });
     grid.setDataProvider(
         (sortOrder, offset, limit) -> employeeService.find(sortOrder, offset, limit),
         () -> employeeService.count());
-    counterLabel.setValue("Size:" + employeeService.count());
+    Random random = new Random(0);
+    grid.setDataProvider(DataProvider.fromCallbacks(
+        query -> IntStream
+                .range(query.getOffset(),
+                        query.getOffset() + query.getLimit())
+                .mapToObj(index -> createPerson(index + 1, random)),
+        query -> 100 * 1000 * 1000));
+
+    counterLabel.setText("Size:" + employeeService.count());
 
     employeeEditor.setChangeHandler((e) -> {
       employeeEditor.setVisible(false);
